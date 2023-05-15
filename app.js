ee6a2e32298825ef020a0eeb2a3fc5c79722279b
Hilel-B <94998820+Hilel-B@users.noreply.github.com>
2023-05-16 00:14:15 +0200

    ajout de card par form

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
 
-const pauvres1 = document.querySelector('.pauvres1');
-const fric1 = document.querySelector('.fric1');
-const argent1 = document.querySelector('.argent1');
-const argent2 = document.querySelector('.argent2');
+// const pauvres1 = document.querySelector('.pauvres1');
+// const fric1 = document.querySelector('.fric1');
+// const argent1 = document.querySelector('.argent1');
+// const argent2 = document.querySelector('.argent2');
+const form = document.querySelector('#formulairenew');
+const cardSection = document.querySelector('#cartes');
+
+let newCards = [];
 
 
-fric1.addEventListener('click', () => {
-    console.log('clicked');
-    let audio = document.querySelector(".mp3-fric1");
-        audio.play();
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    console.log(e, e.target['lien-projet'].value);
+    cardSection.insertAdjacentHTML("beforeend",`<div class="carte">
+                <h3 class="carte__titre">${e.target['titre-projet'].value}</h3>
+                <img class="carte__image" src="${e.target['lien-projet'].value}" />
+                <p class="carte__texte">${e.target['text-projet'].value}</p>
+            </div>`)
 });
-pauvres1.addEventListener('click', () => {
-    console.log('clicked');
-    let audio = document.querySelector(".mp3-pauvres1");
-        audio.play();
-});
-argent1.addEventListener('click', () => {
-    console.log('clicked');
-    let audio = document.querySelector(".mp3-argent1");
-        audio.play();
-});
-argent2.addEventListener('click', () => {
-    console.log('clicked');
-    let audio = document.querySelector(".mp3-argent2");
-        audio.play();
-});
+
+
+// fric1.addEventListener('click', () => {
+//     console.log('clicked');
+//     let audio = document.querySelector(".mp3-fric1");
+//         audio.play();
+// });
+// pauvres1.addEventListener('click', () => {
+//     console.log('clicked');
+//     let audio = document.querySelector(".mp3-pauvres1");
+//         audio.play();
+// });
+// argent1.addEventListener('click', () => {
+//     console.log('clicked');
+//     let audio = document.querySelector(".mp3-argent1");
+//         audio.play();
+// });
+// argent2.addEventListener('click', () => {
+//     console.log('clicked');
+//     let audio = document.querySelector(".mp3-argent2");
+//         audio.play();
+// });
+
+
+
+
+
+
+
