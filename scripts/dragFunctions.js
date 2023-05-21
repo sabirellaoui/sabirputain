f1dd85ef876fb013b34ac939f657553d7d1ec499
Hilel-B <94998820+Hilel-B@users.noreply.github.com>
2023-05-21 20:56:26 +0200

    amelioration drag & drop

--- scripts/dragFunctions.js
+++ scripts/dragFunctions.js
@@ -10,17 +10,30 @@ let dragging = null;
 function dragEnd() {
     dragging = null;
     setTimeout(()=> this.classList.remove('invisible'), 0);
+    for(i=0; i< cardSection.childElementCount; i++){
+        cardSection.children[i].id = i;
+    }
+    saveList();
 }
 
 
 function dragEnter() {
-    if(dragging){
-        dragging.parentNode.insertBefore(dragging, this.nextElementSibling);
+    if(dragging && this.classList.contains('carte')){
+        console.log(this);
+        // dragging.parentNode.insertBefore(dragging, this.nextElementSibling);
+        if(this.id < dragging.id){
+            dragging.parentNode.insertBefore(dragging, this);
+        }else if(this.id > dragging.id){
+            dragging.parentNode.insertBefore(dragging, this.nextElementSibling);
+        }
     }
     // document.getElementById('0').parentNode.insertBefore(document.getElementById('0'), document.getElementById('3'));
 }
 
 
 function dragLeave() {
-    console.log('drag leave', this);
+    // console.log('drag leave', this);
+    for(i=0; i< cardSection.childElementCount; i++){
+        cardSection.children[i].id = i;
+    }
 }
