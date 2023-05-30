536d1f609ecfea9efb013c7622dd61bfe48371a3
sabirellaoui <117686843+sabirellaoui@users.noreply.github.com>
2023-05-30 23:05:04 +0200

    la famille je push un brouillon
    j ai essayé de faire du javascript sans regarder sur internet, pour l instant en chantier la famille, la j ai la flemme je ferai ca plus tard

---
+++ Page2/page2.js
@@ -0,0 +1,23 @@
+const black = document.querySelector('.black')
+const salmon = document.querySelector('.salmon')
+const body = document.querySelector('.body')
+
+black.addEventListener('click', e => {
+    {
+   
+        body.classList.toggle("blackk");
+        console.log("zobi");
+        
+    }
+    }
+  );
+
+  salmon.addEventListener('click', e => {
+    {
+   
+        body.classList.toggle("salmonn");
+        console.log("zobi");
+        
+    }
+    }
+  );
