2b835cc2c359481ff3780f04f1cc9720e16c9bd5
sabirellaoui <117686843+sabirellaoui@users.noreply.github.com>
2023-06-09 22:51:28 +0200

    rajout page 2
    Fond d ecran changement 3 couleurs sur un seule bouton , tu as bien entendu j'ai bien dis 1 seule bouton c est revolutionaire

--- Page2/page2.js
+++ Page2/page2.js
@@ -1,23 +1,33 @@
-const black = document.querySelector('.black')
-const salmon = document.querySelector('.salmon')
-const body = document.querySelector('.body')
 
-black.addEventListener('click', e => {
-    {
-   
-        body.classList.toggle("blackk");
-        console.log("zobi");
-        
-    }
-    }
-  );
 
-  salmon.addEventListener('click', e => {
-    {
+ 
+ const cercle = document.querySelector('.cercle')
+ const body = document.querySelector('.body')
+
+ let clickCount = 0;
+
+cercle.addEventListener('click', e => {
+  clickCount++;
+
+  if (clickCount === 1) {
+    body.classList.add("blackk");
+    console.log("test1");
+  } else if (clickCount === 2) {
+    body.classList.remove("blackk");
+    body.classList.add("salmonn");
+    console.log("test2");
+  } else if (clickCount === 3) {
+    body.classList.remove("salmonn");
+    body.classList.add("blue");
+    console.log("test3");
+
+  } else if (clickCount === 4) {
+    body.classList.remove("blue");
+    body.classList.add("blackk");
+    console.log("test4");
+    clickCount = 1;
    
-        body.classList.toggle("salmonn");
-        console.log("zobi");
-        
-    }
-    }
-  );
+
+
+  }
+});
